6c0a881c576dada650f938edc0ae2fb920b0fbbd
kumari-68 <kumarivelpuri86@gmail.com>
2026-08-04 16:26:44 +0530

    Fix string quotes in runSelenium function

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,5 +12,5 @@ def deployTomcat(jobname,ip,context)
 }
 def runSelenium(jobname)
 {
-  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+  sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
 }
