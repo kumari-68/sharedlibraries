2b77069db9d54aeb974652976455a0f173a7a9b2
kumari-68 <kumarivelpuri86@gmail.com>
2026-09-19 17:00:58 +0530

    Fix deliverytomcat function parameter list

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -15,7 +15,7 @@ def runselenium(jobname)
 {
   sh "java -jar /var/lib/jenkins/workspace/${jobname}/webapp/testing.jar"
 }
-def deliverytomcat(jobname.ip,context)
+def deliverytomcat(jobname,ip,context)
 {
     sh "scp  /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
